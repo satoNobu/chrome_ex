c67e2ee1cf40ee5f732a3ed84eaa035b3e7b17b1
satoNobu <nananiku.s.60@gmail.com>
2022-03-26 23:38:50 +0900

    コンテキストメニュー

--- background.js
+++ background.js
@@ -1,1 +1,14 @@
-alert("background.js");
+chrome.contextMenus.create({title: "hoge", onclick: test}, function(){
+    alert("コンテキストメニューが登録されました");
+});
+
+function test(){alert("クリック")};
+
+// ■ 専用のイベントリスナーの登録
+// chrome.contextMenus.create({title: "hoge"}, function(){
+//     alert("コンテキストメニュー登録完了");
+// });
+//
+// chrome.contextMenus.onClicked.addListener( function(){
+//     alert("onClickedイベントでクリック");
+// });
