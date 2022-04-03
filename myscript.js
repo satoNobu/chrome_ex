8e9c704ef4fc899e81310490bb2f7d6bb1abc189
satoNobu <nananiku.s.60@gmail.com>
2022-04-03 21:57:47 +0900

    コンテンツスクリプト

--- myscript.js
+++ myscript.js
@@ -1,9 +1,5 @@
-let port = chrome.runtime.connect({name: "TestChannel"});
-console.log('myscript読み込み')
-alert('メッセージを送信します')
-port.postMessage({msMessage: "メッセージ送信"});
-
-port.onMessage.addListener(function(msg) {
-    console.log(msg.answerMsg);
-    alert(msg.answerMsg)
+$(function() {
+    $('body').css('background', '#ffc000');
 });
+
+console.log("コンテンツスクリプトからのログ");
